2bc030eaa0e4843bc49f5315fde1adfb34aa9661
Ahmad Haidar Abdullah <ah29mad02@gmail.com>
2022-10-31 09:51:08 +0700

    Comment and Variables in PHP

---
+++ UTS/comment_and_variables.php
@@ -0,0 +1,13 @@
+<?php 
+// Ini Comment Single Line
+
+/*
+ * Ini Comment Multi Lines
+ */
+
+$nama = "Haidar"; // Ini variable nama
+$usia = 20; // Ini variable usia
+
+echo "Halo nama saya $nama, dan usia saya $usia tahun, salam kenal";
+
+?>
