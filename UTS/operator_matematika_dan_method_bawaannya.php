f6009190691b02b0bfff5808644ad094f431143c
Ahmad Haidar Abdullah <ah29mad02@gmail.com>
2022-10-31 10:12:57 +0700

    Operator Aritmatika dan Method Bawaannya

---
+++ UTS/operator_matematika_dan_method_bawaannya.php
@@ -0,0 +1,35 @@
+<?php 
+// Operator Matematika
+//Penjumlahan (+)
+$a = 10;
+$b = 20;
+$c = $a + $b;
+
+// Pengurangan (-)
+$d = $a - $b;
+
+// Perkalian (*)
+$e = $a * $b;
+
+// Pembagian (/)
+$f = $a / $b;
+
+// Modulo / Sisa Bagi (%)
+$g = 13 % $a;
+
+// increment (sama dengan +1)
+$h = 19;
+$h++;
+
+// decrement (sama dengan -1)
+$i = 29;
+$i--;
+
+
+// Method Matematika
+echo "<br>round($p) = ", round($p);
+echo "<br>round($r) = ", round($r);
+echo "<br>max($p, $r) = ", max($p, $r);
+echo "<br>min($p, $r) = ", min($p, $r);
+echo "<br>rand(1,100) = ", rand(1,100);
+?>
