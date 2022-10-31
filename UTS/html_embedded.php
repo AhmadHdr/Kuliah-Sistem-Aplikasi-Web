45538d70a5bf77ddfab97e48272c6dea5d83142b
Ahmad Haidar Abdullah <ah29mad02@gmail.com>
2022-10-31 09:47:29 +0700

    HTML embedded in PHP

---
+++ UTS/html_embedded.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Embedded in PHP</title>
+</head>
+<body>
+    <?= "Halo ini halaman HTML Embedded in PHP" ?>
+</body>
+</html>
