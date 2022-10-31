ab293bd1e20bb580507a8d9b3b15e1bbf9a8ccdc
Ahmad Haidar Abdullah <ah29mad02@gmail.com>
2022-10-31 09:44:11 +0700

    menampilkan data pada php

---
+++ UTS/menampilkan_data.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "Ini menampilkan dengan echo <br>";
+    print "Ini menampilkan dengan print";
+    print_r("Ini menampilkan dengan print_r");
+    var_dump("Ini menampilkan dengan var_dump");
+?>
